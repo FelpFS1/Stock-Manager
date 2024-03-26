659d9c2a53995fb87a10cc120cc35f7c186e3d23
unknown <phff800@gmail.com>
2024-03-26 18:15:15 -0300

    fix password

--- src/components/Alerts/AlertAuth.tsx
+++ src/components/Alerts/AlertAuth.tsx
@@ -13,7 +13,7 @@ export default function AlertAuth({ content }: { content: string }) {
             animate={{ opacity: [0, 1, 1, 0], translateX: [-1000, 0, 0, -1000], }}
             transition={{ duration: 5.6, ease: "easeInOut" }}
             exit={{ translateX: -100, opacity: 0 }}
-            className={showAlert ? " absolute w-10/12 -left-2/4 -right-2/4  bg-red-400 text-center top-16 p-5 rounded-2xl font-bold m-auto" : ""}>
+            className={showAlert ? " absolute w-10/12 -left-2/4 -right-2/4  bg-red-400 text-center top-16 p-5 rounded-2xl font-bold m-auto" : "none"}>
             <p>{content}</p>
         </motion.div>
     )
--- src/services/auth/SignUpForm.ts
+++ src/services/auth/SignUpForm.ts
@@ -16,8 +16,10 @@ export default async function SingUpForm({
   const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$#!%*?&¨])[A-Za-z\d@$#!%*?&¨]+$/;
     const passPassword = passwordRegex.test(password)
     const lengthPassword = password.length >= 8
+    console.log(lengthPassword);
+    
    
-    if(!passPassword && !lengthPassword){  
+    if(!passPassword || !lengthPassword){  
         return {message:"⚠️ A senha deve conter pelo menos 8 caracteres, incluindo pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial. ⚠️"}
     }
     if (password != passwordAgain) return {message:"⚠️ Senhas não correspondem ⚠️"};
